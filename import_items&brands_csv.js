de199614eae4b2d8066462de3abdc60b20eb2d47
stephen rogan <srogan26@gmail.com>
2022-09-14 22:07:04 -0500

    edited the import script and reimported the items and brands. Also, created the express api routes and added them to the router

--- import_items&brands_csv.js
+++ import_items&brands_csv.js
@@ -6,24 +6,24 @@ const path = require("path");
 const brandsCsv = './csv/brands_import.csv'
 const itemsCsv = './csv/items_import.csv'
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Learn65%Big?',
-//     database: 'my_calories_db'
-// });
-const pool = mysql.createPool({
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Learn65%Big?',
-    database: 'my_calories_db',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    database: 'my_calories_db'
 }).promise();
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Learn65%Big?',
+//     database: 'my_calories_db',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// }).promise();
 
 const testQuery = async () => {
-    const result = await pool.query('SELECT * FROM people')
+    const result = await con.query('SELECT * FROM people')
     console.log(result[0]);
 }
 
@@ -31,13 +31,14 @@ const initialImport = async () => {
     await testQuery();
     await importBrands();
     await importItems();
-    pool.end();
+    // pool.end();
+    con.end();
 }
 
 const importBrands = async () => {
     const brandsJson = await csv().fromFile(path.join(__dirname, brandsCsv));
     for (i = 0; i < brandsJson.length; i++) {
-        await pool.query(
+        await con.query(
             'INSERT INTO brands (brand_id, name) VALUES (?,?)',
             [parseInt(brandsJson[i].BRAND_ID), brandsJson[i].BRAND]
         );
@@ -48,7 +49,7 @@ const importItems = async () => {
     const itemsJson = await csv().fromFile(path.join(__dirname, itemsCsv));
     for (i = 0; i < itemsJson.length; i++) {
         const item = itemsJson[i];
-        await pool.query(
+        await con.query(
             'INSERT INTO items (item_id, name, calories, fat, carbs, protein, brand_id) VALUES (?,?,?,?,?,?,?)',
             [
                 parseInt(item.ITEM_ID), 
--- server/Router/router.js
+++ server/Router/router.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const homeRoute = require('./Routes/homeRoute');
+const apiItemsRoute = require('./Routes/api/itemsRoute');
+const apiBrandsRoute = require('./Routes/api/brandsRoute');
+const apiUsersRoute = require('./Routes/api/usersRoute');
+const apiEntryRoute = require('./Routes/api/logEntryRoute');
 
 router.use('/', homeRoute);
+router.use('/api/items', apiItemsRoute);
+router.use('/api/brands', apiBrandsRoute);
+router.use('/api/users', apiUsersRoute);
+router.use('/api/entries', apiEntryRoute);
 
 
 module.exports = router;
